e8f22bdf6887bcd0895f149aa26a8050b1473f5a
User <User@B20>
2023-11-10 11:48:41 +0800

    Add project files.

---
+++ PD6-Module06/DeviceBatteryPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PD6_Module06
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class DeviceBatteryPage : ContentPage
+{
+    public DeviceBatteryPage()
+    {
+        InitializeComponent();
+        SetBackground(Battery.ChargeLevel, Battery.State == BatteryState.Charging);
+    }
+
+    void setBackground(double level, bool charging)
+    {
+        Color? color = null;
+        var status = charging ? "Charging" : "Not Charging";
+        if (level > 0.5f)
+        {
+            color = Color.Green.MultiplyAlpha(level);
+        }
+        if (level > .1)
+        {
+            color = Color.DarkOrange.MultiplyAlpha(level);
+        }
+
+        else
+        {
+            color = Color.Red.MultiplyAlpha(level);
+        }
+
+        BackgroundColor = color.Value;
+        LabelBatteryLevel.Text = status; 
+
+    }
+    void Battery_BatteryInfoChanged(object sender, EventArgs e)
+    {
+            setBackground(e.ChargeLevel, e.state == BatteryState.Charging);
+    }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+
+
+    }
+}
---
+++ PD6-Module06/GeolocationPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PD6_Module06
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class GeolocationPage : ContentPage
+{
+    bool isGettingLocation;
+    public GeolocationPage()
+    {
+        InitializeComponent();
+    }
+
+    async void Start_Locate(System.Object sender, System.EventArgs e)
+    {
+        isGettingLocation = true;
+        while(isGettingLocation)
+        {
+                var result = await GeolocationPage.GetLocationAsync
+                    (new GeolocationRequest(GeolocationAccuracy.Default, TimeSpan.FromMinutes(1)));
+                resultLocation.Text += $"Lat: {result.Latitude}, lng: {result.Longitude} {Environment.NewLine}";
+        }
+    }
+
+    private void Stop_Locate(System.Object sender, System.EventArgs e)
+    {
+            IsGettingLocation = false;
+    }
+}
+}
